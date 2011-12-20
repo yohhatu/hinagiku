ac70d04da4bfd23ff4ffd6c688203b3ab8444668
yohhatu <yohhatu@gmail.com>
2011-12-21 07:43:00 +0900

    add validation of name

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,7 @@ class Task < ActiveRecord::Base
 	scope :search, lambda { |query|
 		where([ "name LIKE ?", "%#{query}%" ])
 	}
+
+	validates :name, :presence => true, :length => { :maximum => 20 }
+	validates :description, :length => { :maximum => 200 }
 end
